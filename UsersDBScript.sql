7428bfa8602d7bdcc015e5dc89afaa5a5beae042
Daniel Aguirre <uhyeay24@gmail.com>
2023-12-11 19:41:31 -0600

    Renamed Database, removed extra whitespace, and made indentations consistent

--- UsersDBScript.sql
+++ UsersDBScript.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE Users;
+CREATE DATABASE TicketMate;
 
-Use Users;
+USE TicketMate;
 
-CREATE TABLE User (
+CREATE TABLE Users (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     Guid CHAR(36) UNIQUE NOT NULL,
     Username NVARCHAR(50) NOT NULL,
@@ -10,29 +10,33 @@ CREATE TABLE User (
     UNIQUE KEY unique_username (Username)
 );
 
-
-
 Create Table Roles (
 	Id INT Auto_Increment Primary Key Not Null,
     Name Nvarchar(32) Not Null
-    );
+);
     
-    Create Table UserRoles (
-		Id Int Auto_Increment Primary Key Not Null,
-        UserId Int not null,
-        RoleId Int not null,
-        Foreign key (UserId) References User(Id),
-        Foreign key (RoleId) References Roles(Id)
-        );
-        
-        INSERT INTO Roles (Name) VALUES ('User'), ('Admin');
+Create Table UserRoles (
+	Id Int Auto_Increment Primary Key Not Null,
+	UserId Int not null,
+	RoleId Int not null,
+	Foreign key (UserId) References User(Id),
+	Foreign key (RoleId) References Roles(Id)
+);
+	
+INSERT INTO Roles (Name) 
+VALUES 
+	('User'), 
+	('Admin')
+;
 
-		INSERT INTO User (Guid, Username, PasswordHash) VALUES
-			('123456', 'testuser1', 'hash1'),
-			('789012', 'testuser2', 'hash2');
+INSERT INTO User (Guid, Username, PasswordHash) 
+VALUES
+	('123456', 'testuser1', 'hash1'),
+	('789012', 'testuser2', 'hash2')
+;
 
-
-		INSERT INTO UserRoles (UserId, RoleId) VALUES
-			(1, 1),
-			(2, 2);
-
+INSERT INTO UserRoles (UserId, RoleId) 
+VALUES
+	(1, 1),
+	(2, 2)
+;
